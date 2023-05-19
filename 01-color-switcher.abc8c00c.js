356e147373a67cba9bf5183bdd01c9660eb21a35
OlenaKruts <120014835+OlenaKruts@users.noreply.github.com>
2023-05-19 12:59:21 +0000

    Deploying to gh-pages from @ OlenaKruts/goit-js-hw-09-01@b4868f5778eb7f4489b1f5c51a8394530c663cc2 🚀

--- 01-color-switcher.abc8c00c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(e){t.setAttribute("disabled","disabled"),r=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(e){t.removeAttribute("disabled"),clearInterval(r)}))}();
-//# sourceMappingURL=01-color-switcher.abc8c00c.js.map
